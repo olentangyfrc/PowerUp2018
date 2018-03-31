e9b572c25be162bf6c046c331e096e1310f0c513
Connor2573 <connor.park2573@gmail.com>
2018-03-31 19:09:25 -0400

    made swerve drive slightly ok, working on making it great again

--- src/org/usfirst/frc/team4611/robot/swerve/SwerveDrive.java
+++ src/org/usfirst/frc/team4611/robot/swerve/SwerveDrive.java
@@ -9,8 +9,23 @@ public class SwerveDrive extends Subsystem{
 	SwerveWheel BLSwerveWheel;
 	SwerveWheel BRSwerveWheel;
 	
-	public SwerveDrive() {
-		
+	public SwerveDrive(int rotFL, int wheelFL, int rotFR, int wheelFR, int rotBL, int wheelBL, int rotBR, int wheelBR) {
+		FLSwerveWheel = new SwerveWheel(rotFL, wheelFL);
+		FRSwerveWheel = new SwerveWheel(rotFR, wheelFR);
+		BLSwerveWheel = new SwerveWheel(rotBL, wheelBL);
+		BRSwerveWheel = new SwerveWheel(rotBR, wheelBR);
+	}
+	public void swerve(double xVal, double yVal, double zVal) {
+		double mag = Math.sqrt(Math.pow(xVal, 2) + Math.pow(yVal, 2));
+		double course = Math.atan(yVal/xVal);
+		turnWheels(course);
+	}
+	public void turnWheels(double dDegree) {
+		int iDegree = (int)dDegree;
+		FLSwerveWheel.turnWheelToDegree(iDegree);
+		FRSwerveWheel.turnWheelToDegree(iDegree);
+		BLSwerveWheel.turnWheelToDegree(iDegree);
+		BRSwerveWheel.turnWheelToDegree(iDegree);
 	}
 	@Override
 	protected void initDefaultCommand() {
