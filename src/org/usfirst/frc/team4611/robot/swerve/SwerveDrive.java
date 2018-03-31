c04a1c4d9f9db01688d88f570131e2d745a15cb7
Ben Hilger <Ben Hilger@DESKTOP-E36PTM7.lan>
2018-03-31 18:50:30 -0400

    Added basic SwerveWheel Control

---
+++ src/org/usfirst/frc/team4611/robot/swerve/SwerveDrive.java
@@ -0,0 +1,20 @@
+package org.usfirst.frc.team4611.robot.swerve;
+
+import edu.wpi.first.wpilibj.command.Subsystem;
+
+public class SwerveDrive extends Subsystem{
+
+	SwerveWheel FLSwerveWheel;
+	SwerveWheel FRSwerveWheel;
+	SwerveWheel BLSwerveWheel;
+	SwerveWheel BRSwerveWheel;
+	
+	public SwerveDrive() {
+		
+	}
+	@Override
+	protected void initDefaultCommand() {
+		
+	}
+
+}
--- src/org/usfirst/frc/team4611/robot/swerve/SwerveWheel.java
+++ src/org/usfirst/frc/team4611/robot/swerve/SwerveWheel.java
@@ -1,5 +1,6 @@
 package org.usfirst.frc.team4611.robot.swerve;
 
+import com.ctre.phoenix.motorcontrol.ControlMode;
 import com.ctre.phoenix.motorcontrol.FeedbackDevice;
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
 
@@ -8,17 +9,56 @@ public class SwerveWheel {
 	private WPI_TalonSRX rotateTalon;
 	private WPI_TalonSRX wheelTalon;
 	
+	private final int FULL_ENCODER_ROTATION = 4092;
+	
+	private int magicValuesAccel = 2000;
+	private int motionmagicCruiseVelocity = 2000;
 	
 	public SwerveWheel(int roT, int whT) {
 		this.rotateTalon = new WPI_TalonSRX(roT);
 		this.wheelTalon = new WPI_TalonSRX(whT); 
 		
-		rotateTalon.configSelectedFeedbackSensor(FeedbackDevice.CTRE_MagEncoder_Relative, 0, 0);
+		rotateTalon.configSelectedFeedbackSensor(FeedbackDevice.CTRE_MagEncoder_Absolute, 0, 0);
+		rotateTalon.setSelectedSensorPosition(0, 0, 0);
+		wheelTalon.configMotionAcceleration(magicValuesAccel, 0);
+		wheelTalon.configMotionCruiseVelocity(motionmagicCruiseVelocity, 0);
+		
 		wheelTalon.configSelectedFeedbackSensor(FeedbackDevice.CTRE_MagEncoder_Relative, 0, 0);
-
+		wheelTalon.setSelectedSensorPosition(0, 0, 0);
+		wheelTalon.configMotionAcceleration(magicValuesAccel, 0);
+		wheelTalon.configMotionCruiseVelocity(motionmagicCruiseVelocity, 0);
+		wheelTalon.config_kP(0, 0.5, 0);
 	}
 	
-	public void swerve(double xVal, double yVal, double zVal) {
+	public void turnWheelToDegree(int desdegree) {
+		int currentOri = rotateTalon.getSelectedSensorPosition(0);
+		
+		double curDeg = ((currentOri/FULL_ENCODER_ROTATION)*360)%360;
+		
+		double err = Math.abs(desdegree-curDeg);
+	
+		double ifRight = err;
+		double ifLeft = err + (360-curDeg); 
+		
+		double posUNeed = 0;
+		if(ifRight > ifLeft) {
+			//Going left
+			posUNeed = (err + (360-curDeg))*FULL_ENCODER_ROTATION;
+		}else if(ifRight < ifLeft) {
+			//Going Right
+			posUNeed = (err/360)*FULL_ENCODER_ROTATION;
+		}
+		
+		rotateTalon.set(ControlMode.MotionMagic, posUNeed);
 		
 	}
+	
+	public void setVelocity(double velocity) {
+		wheelTalon.set(ControlMode.Velocity, velocity);
+	}
+	
+	public void resetEncoder() {
+		wheelTalon.setSelectedSensorPosition(0, 0, 0);
+	}
+	
 }
